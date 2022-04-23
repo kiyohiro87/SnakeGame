23730c4340dad84e5cc729b2273adcaf9ef88e33
Claudio Anagusko <kiyohiro87@gmail.com>
2022-04-23 15:18:26 -0300

    Moving the snake

--- SnakeGame.py
+++ SnakeGame.py
@@ -2,21 +2,52 @@ import pygame
 
 pygame.init()
 display = pygame.display.set_mode((900,600))
-
 pygame.display.set_caption('Snake game by kiyohiro')
 
-blue = (0,0,255)
+white = (255, 255, 255)
+black = (0, 0, 0)
 red = (255,0,0)
 
 game_over = False
+
+# initial position
+x = 300
+y = 300
+
+# starts without movement 
+x_change = 0       
+y_change = 0
+
+clock = pygame.time.Clock()
 
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
 
-    pygame.draw.rect(display,blue,[200,150,10,10])
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -10
+                y_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x_change = 10
+                y_change = 0
+            elif event.key == pygame.K_UP:
+                y_change = -10
+                x_change = 0
+            elif event.key == pygame.K_DOWN:
+                y_change = 10
+                x_change = 0
+
+    x += x_change
+    y += y_change
+    display.fill(white)
+
+    pygame.draw.rect(display, black, [x, y, 15, 15 ])
     pygame.display.update()
+
+    # snake speed
+    clock.tick(20)
  
 pygame.quit()
 quit()
