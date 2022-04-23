65a6b5c421b35d87eb2f9415576b78f5a880cc6a
Claudio Anagusko <kiyohiro87@gmail.com>
2022-04-23 15:08:43 -0300

    Creating the snake

--- SnakeGame.py
+++ SnakeGame.py
@@ -2,8 +2,11 @@ import pygame
 
 pygame.init()
 display = pygame.display.set_mode((900,600))
-pygame.display.update()
+
 pygame.display.set_caption('Snake game by kiyohiro')
+
+blue = (0,0,255)
+red = (255,0,0)
 
 game_over = False
 
@@ -11,6 +14,9 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
+    pygame.draw.rect(display,blue,[200,150,10,10])
+    pygame.display.update()
  
 pygame.quit()
 quit()
